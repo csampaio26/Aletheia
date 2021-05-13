1b1d885c9ddd062395ec7a7489eb35e619cd016b
Carla Sampaio <carla@adapttech.eu>
2021-05-13 20:48:34 +0100

    chore: adding ui

---
+++ AletheiaUI/Models/Argument.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AletheiaUI.Models
+{
+    public class Argument
+    {
+        public Operation Operation { get; set; }
+        public string OutputDirectory { get; set; }
+        public string ProjectPath{ get; set; }
+}
+}
---
+++ AletheiaUI/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using AletheiaUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AletheiaUI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public Argument Argument { get; set; }
+
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Argument = new Argument();
+        }
+
+
+    }
+}
